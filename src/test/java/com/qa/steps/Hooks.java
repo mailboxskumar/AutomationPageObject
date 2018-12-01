a1231d65bec05830bd2131d49299433112f4c13a
mailboxskumar <mailbox.skumar@gmail.com>
2018-12-01 09:23:09 +0530

    Code update in step file

--- src/test/java/com/qa/steps/Hooks.java
+++ src/test/java/com/qa/steps/Hooks.java
@@ -6,32 +6,42 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 
+import com.qa.driver.DriverManager;
+
 import static com.qa.driver.DriverManager.*;
 import static com.qa.utility.Config.*;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
-public class Hooks {
+public class Hooks extends DriverManager{
 
-	public static WebDriver driver;
+	public static WebDriver hdriver;
 	Logger logger = Logger.getLogger(Hooks.class);
 
 	@Before()
 	public void setUpDriver() {
 		logger.info(">>> @Before Hook Step Starts");
 		BasicConfigurator.configure();
-		driver = initializeDriver();
+		
+		hdriver = initializeDriver();
+		hdriver.get(appURL);
+		hdriver.manage().window().maximize();
+		hdriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		
+		/*initializeDriver();
 		driver.get(appURL);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);*/
+		
+		
 		logger.info(">>> @Before Hook Step ends");
 	}
 
 	@After()
 	public void closeDriver() {
 		logger.info(">>> @After Hook Step Starts");
-		driver.quit();
+		hdriver.quit();
 		logger.info(">>> @After Hook Step ends");
 	}
 
